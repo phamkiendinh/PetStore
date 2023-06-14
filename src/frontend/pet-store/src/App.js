bf34f1f8259838bf8fe8f177e810137ada34ee8f
DinhPham <dinhkienn.pham@gmail.com>
2023-06-14 10:49:53 +0700

    Including ReactJS for front-end

---
+++ src/frontend/pet-store/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dinh Dep Trai</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
